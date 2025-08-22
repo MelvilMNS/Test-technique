a338325b596ca33c5e62fd4582db77f6404d79ac
MelvilMNS <127743721+MelvilMNS@users.noreply.github.com>
2025-08-22 16:22:26 +0200

    Afficher en jaune la table réservée

--- frontend/restaurant/src/app/components/table-list/table-list.ts
+++ frontend/restaurant/src/app/components/table-list/table-list.ts
@@ -11,10 +11,15 @@ import { CommonModule } from '@angular/common';
 export class TableList {
   @Input() tables: Table[] = [];
 
+  @Input() reservationId: number | null = null;
+
   ngOnChanges(changes: any) {
     if (changes.tables?.currentValue) {
-      // console.log('Tables updated:', changes.tables.currentValue);
       this.tables = changes.tables.currentValue;
+    }
+
+    if (changes.reservationId?.currentValue) {
+      this.reservationId = changes.reservationId.currentValue;
     }
   }
 }
